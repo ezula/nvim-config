54a526072fb7725ed0c24cb11807ce90d0e7501f
Mikael Svens <svensmikael@gmail.com>
2025-02-10 21:29:16 +0100

    Updates

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -59,8 +59,32 @@ vim.api.nvim_create_autocmd('LspAttach', {
 -- You'll find a list of language servers here:
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
 -- These are example language servers. 
-require('lspconfig').gleam.setup({})
-require('lspconfig').ocamllsp.setup({})
+require('lspconfig').lua_ls.setup({
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using
+        -- (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {
+          'vim',
+          'require'
+        },
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+})
 
 local cmp = require('cmp')
 
--- lua/svunz/plugins/plugins.lua
+++ lua/svunz/plugins/plugins.lua
@@ -1,35 +1,45 @@
-
 return {
 
-	{
-		'nvim-telescope/telescope.nvim', tag = '0.1.8',
-		-- or                              , branch = '0.1.x',
-		dependencies = { 'nvim-lua/plenary.nvim' }
-	}, 
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.8',
+        -- or                              , branch = '0.1.x',
+        dependencies = { 'nvim-lua/plenary.nvim' }
+    },
 
-	{ 
-		"rose-pine/neovim", 
-		name = "rose-pine",
-		config = function()
-			vim.cmd("colorscheme rose-pine")
-		end
-	},
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        config = function()
+            vim.cmd("colorscheme rose-pine")
+        end
+    },
 
-	{ 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
+    { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
 
-	{ 'nvim-treesitter/playground' },
+    { 'nvim-treesitter/playground' },
 
-	{ 'ThePrimeagen/harpoon' },
+    { 'ThePrimeagen/harpoon' },
 
-	{ 'mbbill/undotree' },
+    { 'mbbill/undotree' },
 
-	{'neovim/nvim-lspconfig'},
-	{'hrsh7th/nvim-cmp'},
-	{'hrsh7th/cmp-nvim-lsp'},
+    { 'neovim/nvim-lspconfig' },
+    { 'hrsh7th/nvim-cmp' },
+    { 'hrsh7th/cmp-nvim-lsp' },
 
-	{
-		{ "williamboman/mason.nvim", opts = { ensure_installed = 'prettier' } },
-		"williamboman/mason-lspconfig.nvim",
-		"neovim/nvim-lspconfig",
-	}
+    {
+        { "williamboman/mason.nvim", opts = { ensure_installed = 'prettier' } },
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig",
+    },
+    {
+        "nvim-neo-tree/neo-tree.nvim",
+        branch = "v3.x",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+            "MunifTanjim/nui.nvim",
+            -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
+        }
+    }
 }
