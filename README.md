37c6bfe0df20e2d8853b1a72f7ef62081a0945eb
Mikael Svens <mikael.svens@atg.se>
2025-02-11 14:47:26 +0100

    Small adjustments

--- README.md
+++ README.md
@@ -1,11 +1,23 @@
 # Install
 
-git clone https://github.com/ezula/nvim-config ~/.config/nvim
+## Clone repo
+`git clone https://github.com/ezula/nvim-config ~/.config/nvim`
+
+## Install a nerd font of liking
+`brew install --cask font-<FONT NAME>-nerd-font`
+
+Find a suitable one here: https://www.nerdfonts.com/
+
+## Install ripgrep
+`brew install ripgrep` or `yay -S ripgrep` or similar..
+
 
 ## Setup
 
 Run `:Lazy` to install plugins first time.
+
 Run `:checkhealth lazy` to see that everything is good.
+
 
 GL.
 
--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,5 +1,14 @@
 local builtin = require('telescope.builtin')
+
+-- require('telescope').setup{
+--   defaults = {
+--     file_sorter =  require'telescope.sorters'.get_fzy_sorter,
+--     generic_sorter =  require'telescope.sorters'.get_fzy_sorter,
+--   }
+-- }
+
 vim.keymap.set('n', '<leader>pf', builtin.find_files, { })
+vim.keymap.set('n', '<leader>pb', builtin.buffers, { })
 vim.keymap.set('n', '<C-p>', builtin.git_files, { })
 vim.keymap.set('n', '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
